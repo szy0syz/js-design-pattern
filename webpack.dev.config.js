0585a7c1f0404498b502584e2e204291a61d8ffa
szy0syz <szy0syz@gmail.com>
2018-07-23 23:02:25 +0800

    update 2-2 搭建开发环境2

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname,
     filename: './release/bundle.js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, './release'), // 设置根目录
+    open: true,  // 自动打开浏览器
+    port: 9000
   }
 }
